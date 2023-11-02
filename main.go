4a5c3ff5e3b1159fa1da826646057a0138f0dacf
Isaiah <isaiahmstockton@gmail.com>
2023-11-02 14:22:31 -0400

    host static files

--- main.go
+++ main.go
@@ -14,6 +14,8 @@ func main() {
     http.HandleFunc("/", handler)
 
     err := http.ListenAndServe(":3000", nil)
+    fileSystem := http.FileServer(http.Dir("src/static/"))
+    http.Handle("/src/static/", http.StripPrefix("/src/static/", fileSystem))
     if err != nil {
         fmt.Println("There was an error starting the server:", err.Error())
         os.Exit(1)
