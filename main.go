a34fe4656b74d5f8f0476c5fa0ae50032b98d2b3
ItsCentric <isaiahmstockton@gmail.com>
2023-11-02 15:01:25 -0400

    implement router
    - install gorilla/mux
    - add global router
    - pass router to server
    - handle routes with router instead of http package

--- main.go
+++ main.go
@@ -4,18 +4,22 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+
+	"github.com/gorilla/mux"
 )
+
+var router = mux.NewRouter()
 
 func handler(responseWriter http.ResponseWriter, request *http.Request) {
     fmt.Fprintf(responseWriter, "You requested: %s\n", request.URL.Path)
 }
 
 func main() {
-    http.HandleFunc("/", handler)
+    router.HandleFunc("/", handler)
 
-    err := http.ListenAndServe(":3000", nil)
+    err := http.ListenAndServe(":3000", router)
     fileSystem := http.FileServer(http.Dir("src/static/"))
-    http.Handle("/src/static/", http.StripPrefix("/src/static/", fileSystem))
+    router.Handle("/src/static/", http.StripPrefix("/src/static/", fileSystem))
     if err != nil {
         fmt.Println("There was an error starting the server:", err.Error())
         os.Exit(1)
