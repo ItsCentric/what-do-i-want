ff2f8c4bda0554a45746411dad20e8863354efbc
ItsCentric <isaiahmstockton@gmail.com>
2023-11-03 19:13:49 -0400

    implement file-based router (like next.js or sveltekit)
    - automatically register directories in `src` directory as a route

--- main.go
+++ main.go
@@ -2,21 +2,43 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"io/fs"
+	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/gorilla/mux"
 )
 
 var router = mux.NewRouter()
 
-func handler(responseWriter http.ResponseWriter, request *http.Request) {
-    fmt.Fprintf(responseWriter, "You requested: %s\n", request.URL.Path)
+func registerRoutes() {
+    fs.WalkDir(os.DirFS("src/"), ".", func(path string, directory fs.DirEntry, err error) error {
+        if path == "." {
+            return nil
+        }
+        if err != nil {
+            log.Fatal(err)
+        }
+        if directory.IsDir() {
+            router.HandleFunc("/" + strings.TrimSuffix(path, "/index.html"), httpHandler)
+        } else if path == "index.html" {
+            router.HandleFunc("/", httpHandler)
+        }
+        return nil
+    })
+}
+    
+func httpHandler(responseWriter http.ResponseWriter, request *http.Request) {
+    requestedPage := request.URL.Path + "/index.html"
+    htmlTemplate := template.Must(template.ParseFiles("src/" + requestedPage))
+    htmlTemplate.Execute(responseWriter, nil)
 }
 
 func main() {
-    router.HandleFunc("/", handler)
-
+    registerRoutes()
     err := http.ListenAndServe(":3000", router)
     fileSystem := http.FileServer(http.Dir("src/static/"))
     router.Handle("/src/static/", http.StripPrefix("/src/static/", fileSystem))
