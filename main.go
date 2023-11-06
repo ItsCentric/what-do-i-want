57517e7b6375b0daa628e946add4164e9fa05d26
Isaiah <isaiahmstockton@gmail.com>
2023-11-06 13:16:16 -0500

    add form
    - write markup
    - preform server-side validation
    - return form with errors (if any)

--- main.go
+++ main.go
@@ -3,44 +3,37 @@ package main
 import (
 	"fmt"
 	"html/template"
-	"io/fs"
-	"log"
 	"net/http"
 	"os"
-	"strings"
 
 	"github.com/gorilla/mux"
 )
 
 var router = mux.NewRouter()
 
-func registerRoutes() {
-    fs.WalkDir(os.DirFS("src/"), ".", func(path string, directory fs.DirEntry, err error) error {
-        if path == "." {
-            return nil
-        }
-        if err != nil {
-            log.Fatal(err)
-        }
-        if directory.IsDir() {
-            router.HandleFunc("/" + strings.TrimSuffix(path, "/index.html"), httpHandler)
-        } else if path == "index.html" {
-            router.HandleFunc("/", httpHandler)
-        }
-        return nil
-    })
-}
-    
-func httpHandler(responseWriter http.ResponseWriter, request *http.Request) {
-    requestedPage := request.URL.Path + "/index.html"
-    htmlTemplate := template.Must(template.ParseFiles("src/" + requestedPage))
-    htmlTemplate.Execute(responseWriter, nil)
-}
-
 func main() {
-    registerRoutes()
+    indexTemplate := template.Must(template.ParseFiles("src/index.html"))
     fileSystem := http.FileServer(http.Dir("src/static/"))
     router.PathPrefix("/static/").Handler(http.StripPrefix("/static/", fileSystem))
+
+    router.HandleFunc("/", func(responseWriter http.ResponseWriter, request *http.Request) {
+        requestParameters := request.URL.Query()
+        returnedUrl := Url{ Url: requestParameters.Get("url"), Error: requestParameters.Get("error") }
+        indexTemplate.Execute(responseWriter, returnedUrl)
+    })
+    router.HandleFunc("/api/process-item", func(responseWriter http.ResponseWriter, request *http.Request) {
+        if request.Method != http.MethodPost {
+            indexTemplate.Execute(responseWriter, nil)
+            return
+        }
+        url := &Url{
+            Url: request.PostFormValue("url"),
+        }
+        url.Validate()
+        http.Redirect(responseWriter, request, fmt.Sprintf("/?url=%s&error=%s", url.Url, url.Error), 302)
+    })
+
+    
     err := http.ListenAndServe(":3000", router)
     if err != nil {
         fmt.Println("There was an error starting the server:", err.Error())
---
+++ url.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"regexp"
+)
+
+var urlRegex = regexp.MustCompile(`^(?:(?:https?)://)?(?:www\.)?[a-zA-Z0-9-]+\.[a-zA-Z]+(?::\d+)?(?:/[^/\s$]*)?$`)
+
+type Url struct {
+    Url string
+    Error string
+}
+
+func (url *Url) Validate() bool {
+    match := urlRegex.Match([]byte(url.Url))
+    if match == false {
+        url.Error = "Invalid URL"
+    }
+
+    return len(url.Error) == 0
+}
